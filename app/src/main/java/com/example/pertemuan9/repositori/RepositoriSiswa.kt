112d0afe43c914a558e785dc0c582ceb66411a29
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-27 14:35:55 +0700

    memperbarui repositoriSiswa

--- app/src/main/java/com/example/pertemuan9/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/pertemuan9/repositori/RepositoriSiswa.kt
@@ -1,17 +1,20 @@
 package com.example.pertemuan9.repositori
 
-interface RepositoriSiswa {
-    fun getAllSiswaStream(): Flow<List<Siswa>
+import com.example.pertemuan9.room.Siswa
+import com.example.pertemuan9.room.SiswaDao
+import kotlinx.coroutines.flow.Flow
 
-    suspend fun insertSiswa(siswa : Siswa)
+interface RepositoriSiswa{
 
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+
+    suspend fun insertSiswa(siswa: Siswa)
 }
 
 class OfflineRepositoriSiswa(
     private val siswaDao: SiswaDao
-): RepositoriSiswa {
-    override fun getAllSiswaStream(): Flow<List<Siswa> = siswaDao
-        .getAllSiswa
-    override suspend fun  insertSiswa(siswa : Siswa) = siswaDao
-        .insert(siswa)
+) : RepositoriSiswa {
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
 }
