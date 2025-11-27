43ac262a7bfa0f0a2c947a0bd82bb9d455513aad
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-27 13:39:14 +0700

    Tambah EntryViewModel untuk mengelola state UI siswa dan validasi input.

---
+++ app/src/main/java/com/example/pertemuan9/viewmodel/ex/EntryViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.pertemuan9.viewmodel.ex
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan9.repositori.RepositoriSiswa
+
+class EntryViewModel(private val repositori: RepositoriSiswa): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+    /* Fungsi untuk memvalidasi input*/
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa
+        .detailSiswa ): Boolean {
+        return with(uiState){
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank
+        }
+    }
+}
