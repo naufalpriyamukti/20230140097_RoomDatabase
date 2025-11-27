67e9c92af2b527046b5382421bf7901f6d13fca4
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-27 12:54:01 +0700

    Menambah variabel di class siswa

--- app/src/main/java/com/example/pertemuan9/room/Siswa.kt
+++ app/src/main/java/com/example/pertemuan9/room/Siswa.kt
@@ -3,4 +3,8 @@ package com.example.pertemuan9.room
 @Entity(tableName = "tblSiswa")
 data class Siswa(
     @PrimaryKey(autoGenerate = true)
+    val id : Int = 0,
+    val nama : String,
+    val alamat : String,
+    val telpon : String
 )
