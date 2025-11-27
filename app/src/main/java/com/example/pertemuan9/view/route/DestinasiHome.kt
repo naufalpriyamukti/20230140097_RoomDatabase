bfd2594536206b17647d6f3cb4ccdc4b866a2ff7
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-27 13:30:03 +0700

    Menambah objek DestinasiHome untuk navigasi dengan rute dan judul aplikasi.

---
+++ app/src/main/java/com/example/pertemuan9/view/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.pertemuan9.view.route
+
+import com.example.pertemuan9.R
+
+object DestinasiHome : DestinasiNavigasi{
+    overide val route = "home"
+
+    override val tittleRes = R.string.app_name
+}
