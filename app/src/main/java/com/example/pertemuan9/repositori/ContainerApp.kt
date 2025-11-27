35d69ee5dd5369313655997d582f634f40ef22b7
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-27 13:19:29 +0700

    Menambah kelas ContainerDataApp untuk mengelola penyediaan repositori siswa.

--- app/src/main/java/com/example/pertemuan9/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/pertemuan9/repositori/ContainerApp.kt
@@ -1,5 +1,15 @@
 package com.example.pertemuan9.repositori
 
+import com.example.pertemuan9.room.DatabaseSiswa
+
 interface ContainerApp {
     val repositoriSiswa : RepositoriSiswa
 }
+
+class ContainerDataApp(private val context: Context):
+        ContainerApp {
+            override val repositoriSiswa: RepositoriSiswa by lazy {
+                OfflineRepositoriSiswa(
+                    siswaDao = DatabaseSiswa.getDatabase(context).siswaDao())
+            }
+        }
