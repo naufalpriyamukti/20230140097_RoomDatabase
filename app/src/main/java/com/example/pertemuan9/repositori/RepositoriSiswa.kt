9ebbc1b166f3c263e8a386e55dabc8abf48e2eb3
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-12-04 11:43:51 +0700

    implementasi getSiswaStream dan deleteSiswa di class OfflineRepositoriSiswa

--- app/src/main/java/com/example/pertemuan9/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/pertemuan9/repositori/RepositoriSiswa.kt
@@ -25,6 +25,8 @@ class OfflineRepositoriSiswa(
     private val siswaDao: SiswaDao
 ) : RepositoriSiswa {
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
-
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
 }
